500100eeba70d58376544d326492f72a7ec08696
oysteinwangen <oysteinwangen95@gmail.com>
2021-11-28 23:20:47 +0100

    Content types

--- config/database.js
+++ config/database.js
@@ -4,12 +4,12 @@ module.exports = ({ env }) => ({
     default: {
       connector: "mongoose",
       settings: {
-        host: env("DATABASE_HOST", "cluster0.l3a7u.mongodb.net"),
+        host: env("DATABASE_HOST"),
         srv: env.bool("DATABASE_SRV", true),
         port: env.int("DATABASE_PORT", 27017),
-        database: env("DATABASE_NAME", "strapi-sp2-ow"),
-        username: env("DATABASE_USERNAME", "oystein"),
-        password: env("DATABASE_PASSWORD", "strapi-sp2"),
+        database: env("DATABASE_NAME"),
+        username: env("DATABASE_USERNAME"),
+        password: env("DATABASE_PASSWORD"),
       },
       options: {
         authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
