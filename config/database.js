52ebf4a3550c14cac953e115b96384cce3cf127d
oysteinwangen <oysteinwangen95@gmail.com>
2021-11-28 23:01:22 +0100

    databse

--- config/database.js
+++ config/database.js
@@ -4,12 +4,12 @@ module.exports = ({ env }) => ({
     default: {
       connector: "mongoose",
       settings: {
-        host: env("DATABASE_HOST"),
+        host: env("DATABASE_HOST", "cluster0.l3a7u.mongodb.net"),
         srv: env.bool("DATABASE_SRV", true),
         port: env.int("DATABASE_PORT", 27017),
-        database: env("DATABASE_NAME"),
-        username: env("DATABASE_USERNAME"),
-        password: env("DATABASE_PASSWORD"),
+        database: env("DATABASE_NAME", "strapi-sp2-ow"),
+        username: env("DATABASE_USERNAME", "oystein"),
+        password: env("DATABASE_PASSWORD", "strapi-sp2"),
       },
       options: {
         authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
@@ -18,24 +18,3 @@ module.exports = ({ env }) => ({
     },
   },
 });
-
-/*module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'mongoose',
-      settings: {
-        host: env('DATABASE_HOST', 'cluster0.l3a7u.mongodb.net'),
-        srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'strapi-sp2-ow'),
-        username: env('DATABASE_USERNAME', 'oystein'),
-        password: env('DATABASE_PASSWORD', 'strapi-sp2'),
-      },
-      options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', true),
-      },
-    },
-  },
-});*/
